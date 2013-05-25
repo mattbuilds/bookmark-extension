187eb97199f4e6a761d466e3ef9bbb7cf41b7f4d
Tr1cks <matthew.james.mccarthy@gmail.com>
2013-05-24 21:48:11 -0400

    Links are saveable

--- main.js
+++ main.js
@@ -2,13 +2,20 @@ var folders = localStorage["folders"];
 
 $(document).ready(function(){
 	if (folders===undefined){
-		folders = [];
+		folders = {};
 	}
 	$("#create_folder").hide();
 	$("#create_link").hide();
+	$("#display_links").hide();
 	DisplayFolders(folders);
 
+	$("#folders").on('click', 'div.folder', function(){
+		var folderId = $(this).text();
+		FolderPage(folders, folderId);
+	});
+
 	$("#add_folder").click(function(){
+		$("#folders").hide();
 		$("#create_folder").show();
 		$("#add_folder").hide();
 		$("#add_link").hide();	
@@ -17,21 +24,36 @@ $(document).ready(function(){
 	$("#submit_folder").click(function(){
 		var name = $("#folder_name").val();
 		CreateFolder(folders, name);
+		$("#folders").show();
 		$("#create_folder").hide();
 		$("#add_folder").show();
 		$("#add_link").show();	
 	});
 
+	$("#display_links").on('click', 'button.back', function(){
+		$("#folders").show();
+		$("#create_folder").hide();
+		$("#display_links").hide();
+		$("#add_folder").show();
+		$("#add_link").show();		
+	});
+
 	$("#add_link").click(function(){
 		SetCreateLink(folders);
+		$("#folders").hide();
 		$("#create_link").show();
 		$("#add_folder").hide();
 		$("#add_link").hide();
 	});
 
 	$("#submit_link").click(function(){
+		var folder = $("#folder_select").val();
+		var title = $("#link_title").val();
+		var link = $("#link").val();
+		CreateLink(folders, folder, title, link);
+		$("#folders").show();
 		$("#create_link").hide();
 		$("#add_folder").show();
 		$("#add_link").show();	
-	})
+	});
 });
--- popup.js
+++ popup.js
@@ -1,28 +1,66 @@
 function CreateFolder(folders, name){
-  	folders.push(name);
+  folders[name] = [];
 	DisplayFolders(folders);
 }
 
 function DisplayFolders(folders){
 	$("#folders").html('');
-	for (i=0; i<folders.length; i++){
-  		$("#folders").append(
-  			"<div id=folder_"+ folders[i] +">" + folders[i]+"</div>"
-  		);
-  	}
+	for (var key in folders){
+  	$("#folders").append(
+  		"<div class='folder' id=folder_"+ key +">" + key +"</div>"
+  	);
+  }
+  console.log(folders);
+}
+
+function FolderPage(folders, folderId){
+  for(var key in folders){
+    if (key === folderId){
+      folder = folders[key];
+      DisplayLinks(key, folder);
+    }
+  }
+}
+
+function DisplayLinks(link, folder){
+  $("#folders").hide();
+  $("#add_folder").hide();
+  $("#display_links").show();
+  $("#display_links").html('');
+  $("#display_links").append(
+    "<div>" + link +"</div>"
+  );
+  console.log(folder);
+  for (i=0; i < folder.length; i++){
+    $("#display_links").append(
+      "<div>" + folder[i].title +"</div>"
+    );
+  }
+  $("#display_links").append(
+    "<button class=back>Back</button>"
+  )
 }
 
 function SetCreateLink(folders){
 	$("#folder_select").html('');
-	for (i=0; i<folders.length; i++){
-		$("#folder_select").append(
-			"<option value="+folders[i]+">"+folders[i]+"</option>"
+	for (var key in folders){
+	    $("#folder_select").append(
+			"<option value="+ key +">"+ key +"</option>"
 		);
 	}
+  chrome.tabs.getSelected(null, function(tab){
+    $("#link_title").val(tab.title);
+    $("#link").val(tab.url);
+  })
 }
 
-function CreateLink(folders){
-
+function CreateLink(folders, folder, title, link){
+  for (var key in folders){
+    if (key === folder){
+      //console.log(folders);
+      folders[key].push({"title": title, "link" : link});
+    }
+  }
 }
 
 function SetPath(){
@@ -31,4 +69,3 @@ function SetPath(){
     localStorage["path"] = pathname;
   })
 }
-
